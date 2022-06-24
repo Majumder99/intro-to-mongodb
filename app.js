07b83c31488608a2a0b8603371d15bede7b64258
Sourav Majumder <96306119+Majumder99@users.noreply.github.com>
2022-06-24 20:12:09 +0600

    new for connection

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { ObjectId } = require("mongodb");
 
 //init app & middleware
 
@@ -12,11 +13,35 @@ connectToDb((err) => {
     app.listen(3000, () => {
       console.log("App listening on port 3000");
     });
+    db = getDb();
   }
-  db = getDb();
 });
 
 //routes
 app.get("/books", (req, res) => {
-  res.json({ msg: "welcome to the api" });
+  let books = [];
+
+  db.collection("books")
+    .find()
+    .sort({ author: 1 })
+    .forEach((book) => books.push(book))
+    .then(() => {
+      res.status(200).json(books);
+    })
+    .catch((err) => res.status(500).json({ err }));
+
+  // res.json({ msg: "welcome to the api" });
+});
+
+app.get("/books/:id", (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection("books")
+      .findOne({ _id: ObjectId(req.params.id) })
+      .then((doc) => {
+        res.status(200).json(doc);
+      })
+      .catch((err) => res.status(500).json({ err }));
+  } else {
+    res.status(500).json("Id is not valid");
+  }
 });
--- db.js
+++ db.js
@@ -1,18 +1,48 @@
-const { MongoClient } = require("mongodb");
+const MongoClient = require("mongodb").MongoClient;
 
 let dbConnection;
 
+// module.exports = {
+//   connectToDb: (cb) => {
+//     MongoClient.connect("mongodb://localhost:27017/bookcollection")
+//       .then((client) => {
+//         dbConnection = client.db();
+//         return cb();
+//       })
+//       .catch((err) => {
+//         console.log(err);
+//         return cb(err);
+//       });
+//   },
+//   getDb: () => dbConnection,
+// };
+
+const url = "mongodb://127.0.0.1:27017";
+const dbName = "bookcollection";
+let db;
+
+// MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+//   if (err) return console.log(err);
+
+//   // Storing a reference to the database so you can use it later
+//   dbConnection = client.db(dbName);
+//   console.log(`Connected MongoDB: ${url}`);
+//   console.log(`Database: ${dbName}`);
+// });
+
 module.exports = {
   connectToDb: (cb) => {
-    MongoClient.connect("mongodb://localhost:27017/bookcollection")
-      .then((client) => {
-        dbConnection = client.db();
-        return cb();
-      })
-      .catch((err) => {
-        console.log(err);
+    MongoClient.connect(url, { useNewUrlParser: true }, (err, client) => {
+      if (err) {
         return cb(err);
-      });
+      }
+
+      // Storing a reference to the database so you can use it later
+      dbConnection = client.db(dbName);
+      console.log(`Connected MongoDB: ${url}`);
+      console.log(`Database: ${dbName}`);
+      return cb();
+    });
   },
   getDb: () => dbConnection,
 };
