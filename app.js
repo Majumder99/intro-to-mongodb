f416c2c31d7c1520ede92869d6b948a12a9e2351
Sourav Majumder <96306119+Majumder99@users.noreply.github.com>
2022-06-25 02:12:41 +0600

    new for get, post, patch, delete

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const { ObjectId } = require("mongodb");
 const app = express();
 const { connectToDb, getDb } = require("./db");
 
+app.use(express.json());
 //db connection
 let db;
 connectToDb((err) => {
@@ -37,6 +38,44 @@ app.get("/books/:id", (req, res) => {
   if (ObjectId.isValid(req.params.id)) {
     db.collection("books")
       .findOne({ _id: ObjectId(req.params.id) })
+      .then((doc) => {
+        res.status(200).json(doc);
+      })
+      .catch((err) => res.status(500).json({ err }));
+  } else {
+    res.status(500).json("Id is not valid");
+  }
+});
+
+app.post("/books", (req, res) => {
+  const book = req.body;
+
+  db.collection("books")
+    .insertOne(book)
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((err) => res.status(500).json(err));
+});
+
+app.delete("/books/:id", (req, res) => {
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection("books")
+      .deleteOne({ _id: ObjectId(req.params.id) })
+      .then((doc) => {
+        res.status(200).json(doc);
+      })
+      .catch((err) => res.status(500).json({ err }));
+  } else {
+    res.status(500).json("Id is not valid");
+  }
+});
+
+app.patch("/books/:id", (req, res) => {
+  const book = req.body;
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection("books")
+      .updateOne({ _id: ObjectId(req.params.id) }, { $set: book })
       .then((doc) => {
         res.status(200).json(doc);
       })
